6fc10bf1e07aaa459cdc4758ae276aa3c7725a48
fl0cke <janb.l33t@gmail.com>
2018-05-09 18:09:30 +0200

    fix button toggle label import

--- src/components/VBtnToggle/index.js
+++ src/components/VBtnToggle/index.js
@@ -1,8 +1,12 @@
 import VBtnToggle from './VBtnToggle'
+import VBtnToggleLabel from "./VBtnToggleLabel";
+
+export { VBtnToggle, VBtnToggleLabel }
 
 /* istanbul ignore next */
 VBtnToggle.install = function install (Vue) {
   Vue.component(VBtnToggle.name, VBtnToggle)
+  Vue.component(VBtnToggleLabel.name, VBtnToggleLabel)
 }
 
 export default VBtnToggle
